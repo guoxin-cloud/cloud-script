c87c5f39153806c64fa7901906873818384c52a9
yuqing <yuqing@91guoxin.com>
2016-07-12 17:15:51 +0800

    yuqing-case

--- Cloud_API_Regression_Test/Cloud_API_Regression_Test/run_api_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pybot --variable API_VERSION:$API_VERSION --variable API_ROOT_URL:$API_ROOT_URL --outputdir ./logs --exclude $EXCLUDE1 /Users/guanhua.jing/.jenkins/jobs/guoxin/workspace/Cloud_API_Regression_Test
-exit 0
